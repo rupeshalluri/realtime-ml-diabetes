ef2d2f04e79c05522d624c51e2b13d4cc147599e
rupeshalluri <83739527+rupeshalluri@users.noreply.github.com>
2025-05-15 16:39:07 +0530

    Update app.py

--- docker/app.py
+++ docker/app.py
@@ -3,8 +3,7 @@ from fastapi import FastAPI, Request
 import pandas as pd
 
 app = FastAPI()
-model = mlflow.pyfunc.load_model("model")
-
+model = mlflow.pyfunc.load_model("/home/runner/work/realtime-ml-diabetes/realtime-ml-diabetes/saved_model")
 @app.post("/predict")
 async def predict(request: Request):
     payload = await request.json()
